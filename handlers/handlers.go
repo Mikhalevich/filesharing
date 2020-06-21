9f9c8f5e06664b75a4d43ffb9c7390ece2483c05
Michael Mikhalevich <m.mikhalevich@melsoft-games.com>
2020-06-21 13:07:17 +0300

    content disposition header for download files was added

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -162,6 +162,8 @@ func (h *Handlers) GetFileHandler(w http.ResponseWriter, r *http.Request) {
 		pw.CloseWithError(err)
 	}()
 
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", sp.FileName))
+
 	_, err = io.Copy(w, pr)
 	if h.respondWithError(err, w, "GetFileHandler", "can't open file", http.StatusInternalServerError) {
 		return
